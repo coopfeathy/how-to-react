4ba956797afc8520ed84d6b0c2108791462f469f
coopfeathy <44649389+coopfeathy@users.noreply.github.com>
2020-01-31 13:10:18 -0600

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react"
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Navbar, Nav, NavDropdown, Form, FormControl, Button, NavLink} from 'react-bootstrap';
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "./index.css"
+import {Navbar, Nav, NavDropdown, Form, FormControl, Button, CardColumns, Card} from 'react-bootstrap';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-import AllCards from "./cards/allCards";
+import {cards} from './cards/cardsdata';
+import logo from './pictures/logo.webp'
+
 import CraftCards from "./cards/craftCards";
 import ContactForm from "./contact";
 
@@ -15,27 +17,26 @@ class App extends Component {
       <div className="App">
         <Router>
           <Navbar bg="dark" variant="dark" expand="lg">
-            <Navbar.Brand>How To</Navbar.Brand>
+            <Navbar.Brand>
+              <img alt="" src={logo} width="30" height="30" className="d-inline-block align-top" />{' '} How To
+            </Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="mr-auto">
 
                 <Nav.Link ><Link className="link" to="/all">See All</Link></Nav.Link>
-
-                <Nav.Link><Link className="link" to="/contact">Contact</Link></Nav.Link>
-
                 <NavDropdown title="Filter" id="basic-nav-dropdown">
-
                   <NavDropdown.Item><Link className="dropLink" to="/crafts">Crafts</Link></NavDropdown.Item>
-                  
-                </NavDropdown>
+                </NavDropdown>  
+                <Nav.Link><Link className="link" to="/contact">Contact</Link></Nav.Link>
               </Nav>
               <Form inline>
                 <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-                <Button variant="outline-success">Search</Button>
+                <Button variant="outline-success"><Link className="link" to="/search">Search</Link></Button>
               </Form>
             </Navbar.Collapse>
           </Navbar>
+          
           <br/>
           <Switch>
             <Route exact path="/all">
@@ -47,11 +48,36 @@ class App extends Component {
             <Route path="/crafts">
               <CraftCards />
             </Route>
+            <Route path="/search">
+              <CraftCards />
+            </Route>
           </Switch>
         </Router>
       </div>
     )
   }
+}
+
+function AllCards() {
+  let listCards = cards.map((x)=>
+    <Card key={x.id} bg={x.color} text="white" style={{ width: '18rem' }}>
+      <Card.Img variant="top" src={x.image} />
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the bulk of
+          the card's content.
+        </Card.Text>
+        <Button variant="light">Learn Here</Button>
+      </Card.Body>
+    </Card>
+  );
+
+  return (
+    <CardColumns>
+      {listCards}
+    </CardColumns>
+  )
 }
 
 export default App
---
+++ src/cards/cardsdata.js
@@ -0,0 +1,23 @@
+import image1 from '../pictures/yes.png'
+
+var cards = [
+    {
+        "id": 1,
+        "color":"primary",
+        "filter": "food",
+        "image": image1,
+        "title":"Card title",
+        "description":"eeeeeeet",
+        "link":"cooperfeatherstone.com"
+     },
+     {
+       "id": 2,
+       "color":"warning",
+       "filter": "craft",
+       "image": "pic with width of 286",
+       "title":"Card title",
+       "description":"this is a test",
+       "link":"cooperfeatherstone.com"
+    }
+];
+export {cards};
